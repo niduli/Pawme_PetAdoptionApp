555500ea576b973f3aeb8df9c290530f1234b6ce
SavindyaPramudith <savindyapramudith@gmail.com>
2025-06-26 12:15:51 +0530

    Create Foster List,login and Registration

---
+++ app/src/main/java/com/example/pawmepetadoptionapp/FosterRegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pawmepetadoptionapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FosterRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foster_registration)  // This links to your XML UI layout
+    }
+}
