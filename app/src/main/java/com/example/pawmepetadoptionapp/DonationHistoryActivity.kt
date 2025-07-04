6ab32e6f8b5aaf840f0dcf9c3f9a3e622bce3e89
Kushmi Hettiwaththage <meheraankushmi@gmail.com>
2025-07-04 21:59:03 +0530

    created activity_donation.xml,activity_money_donation.xml,activity_in_kind_donation.xml,activity_payment.xml

---
+++ app/src/main/java/com/example/pawmepetadoptionapp/DonationHistoryActivity.kt
@@ -0,0 +1,38 @@
+package com.example.pawmepetadoptionapp
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.yourapp.R
+
+class DonationHistoryActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: DonationAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.donation_history)
+
+        // Back icon click
+        findViewById<ImageView>(R.id.backIcon).setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        recyclerView = findViewById(R.id.donationRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Sample donation history
+        val donationList = listOf(
+            DonationModel("Money Donation", "15/01/2025", "$100"),
+            DonationModel("In-Kind Donation", "15/01/2025", "$100"),
+            DonationModel("Money Donation", "15/01/2025", "$100"),
+            DonationModel("Money Donation", "15/01/2025", "$100")
+        )
+
+        adapter = DonationAdapter(donationList)
+        recyclerView.adapter = adapter
+    }
+}
