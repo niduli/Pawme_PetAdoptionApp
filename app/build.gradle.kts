0a4199e92a2286e14b5904d4f92dd9c701ae0877
Anushi Shehara <anushidesilva>
2025-07-30 11:37:45 -0700

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation(libs.glide)
-    kapt(libs.glideCompiler)
-    implementation(libs.google.maps)
-    implementation(libs.mpandroidchart)
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    kapt ("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
