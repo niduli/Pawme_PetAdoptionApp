f0ebda2e82abb2a4ffb0839b2ffe4fef88a4373c
Kushmi Hettiwaththage <meheraankushmi@gmail.com>
2025-07-30 15:25:23 +0530

    fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("com.google.android.material:material:1.5.0")
+
 }
