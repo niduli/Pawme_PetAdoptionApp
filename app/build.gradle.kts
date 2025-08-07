74d05fbac9cfd4db1f12ec20618cbd3c9953633b
Anushi Shehara <anushidesilva>
2025-08-07 15:17:20 -0700

    fixed conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,9 +69,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("androidx.activity:activity-ktx:1.8.2")
     implementation ("androidx.core:core-ktx:1.12.0")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.cloudinary:cloudinary-android:2.3.1")
 
 
 }
